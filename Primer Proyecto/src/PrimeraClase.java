914941ee2b32fa00a0a6c6150caefc4801c61270
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-02-02 10:28:50 +0100

    Copia del workspace anterior

---
+++ Primer Proyecto/src/PrimeraClase.java
@@ -0,0 +1,9 @@
+
+public class PrimeraClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola este es mi primer Class en Eclipese");
+	}
+
+}
