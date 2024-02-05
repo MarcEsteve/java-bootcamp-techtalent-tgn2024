abddf5e07b2560b13095a388bb233907436625ba
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-02-05 13:26:51 +0100

    Ejericio 2

--- Pruebas Operadores/src/Ejercicio1T04App.java
+++ Pruebas Operadores/src/Ejercicio1T04App.java
@@ -1,4 +1,4 @@
-public class Ejercicio1T04 {
+public class Ejercicio1T04App {
 	public static void main(String[] args) {
 		// Declaración de variables
 		int numero1 = 15;
---
+++ Pruebas Operadores/src/Ejercicio2T04App.java
@@ -0,0 +1,20 @@
+public class Ejercicio2T04App {
+    public static void main(String[] args) {
+        // Declaración e inicialización de variables
+        int N = 5;
+        double A = 4.56;
+        char C = 'a';
+
+        // Muestra el valor de cada variable
+        System.out.println("Variable N = " + N);
+        System.out.println("Variable A = " + A);
+        System.out.println("Variable C = " + C);
+
+        // Muestra la suma de N + A y la diferencia de A - N
+        System.out.println(N + " + " + A + " = " + (N + A));
+        System.out.println(A + " - " + N + " = " + (A - N));
+
+        // Muestra el valor numérico correspondiente al carácter en la variable C
+        System.out.println("Valor numérico del carácter " + C + " = " + (int) C);
+    }
+}
