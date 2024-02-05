be5c6048828e0f4e08ca9b486e08f7f8eb817849
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-02-05 10:24:23 +0100

    Prueba operador relacion

---
+++ Pruebas Operadores/src/PruebasRelaciones.java
@@ -0,0 +1,38 @@
+
+public class PruebasRelaciones {
+
+	    public static void main(String[] args) {
+	        // TODO Auto-generated method stub
+	        int operador1=10;
+	        int operador2=20;
+	        int operador3=10;
+	        boolean resultado1;
+	        boolean resultado2;
+	        boolean resultado3;
+	        boolean resultado4;
+	        boolean resultado5;
+	        boolean resultado6;
+	        boolean resultado7;
+	        boolean resultado8;
+
+	        resultado1=operador1==operador2; //¿10 es igual a 20? resultado=false
+	        resultado2=operador1==operador3; //¿10 es igual a 10? resultado=true
+	        resultado3=operador1!=operador2; //¿10 es distinto a 20? resultado=true
+	        resultado4=operador1>operador2; //¿10 es mayor que 20? resultado=false
+	        resultado5=operador1>operador3; //¿10 es mayor que 10? resultado=false
+	        resultado6=operador1<operador2; //¿20 es mayor que 10? resultado=true
+	        resultado7=operador1<=operador3; //¿10 es mayor o igual que 10? resultado=true
+	        resultado8=operador1>=operador2; //¿10 es mayor o igual que 20? resultado=false
+
+	        System.out.println("¿" + operador1 + " es igual a " + operador2 + "? " + resultado1);
+	        System.out.println("¿10 es igual a 10? " +resultado2);
+	        System.out.println("¿10 es distinto a 20? " +resultado3);
+	        System.out.println("¿10 es mayor que 20? " +resultado4);
+	        System.out.println("¿10 es mayor que 10? " +resultado5);
+	        System.out.println("¿20 es mayor que 10? " +resultado6);
+	        System.out.println("¿10 es mayor o igual que 10? " +resultado7);
+	        System.out.println("¿10 es mayor o igual a 20? " +resultado8);
+
+	    }
+
+	}
