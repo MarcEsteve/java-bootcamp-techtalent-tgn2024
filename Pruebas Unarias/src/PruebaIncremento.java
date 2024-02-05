9ae2751d13452a5e952aca852c56fa95af610222
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-02-05 10:01:52 +0100

    Pruebas Unarias Incremente decremento

---
+++ Pruebas Unarias/src/PruebaIncremento.java
@@ -0,0 +1,26 @@
+
+public class PruebaIncremento {
+
+	public static void main(String[] args) {
+		
+		//Operadores unarios incremento ++ decremento --
+		int operador = 10;
+		int x=0;
+		operador++;
+		operador++;
+		operador--;
+		
+		x=operador++;
+		
+		x=++operador;	
+		
+		//EJERCICIO
+		
+		int op = 10;
+		System.out.println(op);
+		System.out.println(op++);
+		System.out.println(++op);
+
+	}
+
+}
