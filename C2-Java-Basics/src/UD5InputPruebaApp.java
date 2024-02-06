de2b90c7111a7c3af32cba52ec0fc6c34327e08f
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-02-06 10:36:05 +0100

    intro teoria UD5

---
+++ C2-Java-Basics/src/UD5InputPruebaApp.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class UD5InputPruebaApp {
+
+	public static void main(String[] args) {
+
+		String nombre = JOptionPane.showInputDialog("Introduce tu nombre");
+		
+		JOptionPane.showInternalMessageDialog(null, 
+				"Bien, tu nombre es " + nombre);
+
+	}
+
+}
---
+++ C2-Java-Basics/src/UD5PruebaApp.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class UD5PruebaApp {
+
+	public static void main(String[] args) {
+
+		int precioProducto = 300;
+		final double IVA = 0.21;
+
+		JOptionPane.showMessageDialog(null,
+						"El precio del producto, incluido el IVA es " + 
+						(precioProducto + (precioProducto * IVA)));
+
+	}
+
+}
