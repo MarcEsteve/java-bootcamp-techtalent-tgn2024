35355663d12d6dac04c023b4ba88cfebac1167fb
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-02-14 10:31:33 +0100

    Update UD6MetodosApp.java

--- C2-Java-Basics/src/UD6MetodosApp.java
+++ C2-Java-Basics/src/UD6MetodosApp.java
@@ -3,20 +3,27 @@ public class UD6MetodosApp {
 
 	public static void main(String[] args) {
 		int operador1=-6, operador2=5;
+		int resultado = sumaNumeros(operador1, operador2);
+		System.out.println("El resultado de la suma es: " + resultado);
 		
-		if (sumaNumeros(operador1, operador2)) {
+		if (positivos(resultado)) {
 			System.out.println("El resultado es mayor o igual 0");
 		} else {
 			System.out.println("El resultado es negativo");
 		}
-
 	}
 	
-	public static boolean sumaNumeros(int num1, int num2) {
+	public static int sumaNumeros(int num1, int num2) {
+		return num1+num2;
+		//Ejercicio: divide este método sumaNumeros() en dos,
+		//El primero se llama sumaNumeros(int num1, int num2) y
+		//Devuelve un int del cálculo de la suma
+		//El segundo se llama  postivo(int param1) pero devuelve
+		//un booleano 
+	}
+	public static boolean positivos(int param1) {
 		
-		int resultado = num1+num2;
-		
-		if (resultado >=0) {
+		if (param1 >=0) {
 			return true;
 		} else {
 			return false;
@@ -27,7 +34,6 @@ public class UD6MetodosApp {
 		//El segundo se llama  postivo(int param1) pero devuelve
 		//un booleano 
 	}
-	
 	
 
 }
