5e0f0a2c6c015c6da81c494a65faad666be6035f
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-02-15 12:22:21 +0100

    Create UD6ArrayMultApp.java

---
+++ C2-Java-Basics/src/UD6MetodosArrays/UD6ArrayMultApp.java
@@ -0,0 +1,14 @@
+package UD6MetodosArrays;
+
+public class UD6ArrayMultApp {
+
+	public static void main(String[] args) {
+			
+		//Declaramos un array de 3 filas x 5 columnas
+		int array[][] = new int [3][5];
+		array[0][1] = 5;
+		array[1][3] = 6; 
+		
+	}
+
+}
