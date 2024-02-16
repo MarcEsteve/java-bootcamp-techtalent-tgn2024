6fa42df53c1113ff234eaa6341bb91affa8212cc
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-02-16 11:40:17 +0100

    Update T6EPalindromoApp.java

--- C2-Java-Basics/src/UD6MetodosArrays/T6EPalindromoApp.java
+++ C2-Java-Basics/src/UD6MetodosArrays/T6EPalindromoApp.java
@@ -5,33 +5,41 @@ public class T6EPalindromoApp {
 	public static void main(String[] args) {
 		// Ejercicio: escribe un programa que devuelva
 		// si un String es palíndromo Ejemplo:"sometemos" o "aibofobia"
+		
+		//Declaración e Inicialización de variables
 		String palin = "amoroooma";
 		String palinReverse = "";
-		int indiceRecorre = palin.length() - 1; // 8
-		System.out.println(indiceRecorre);
-		char caracter = palin.charAt(indiceRecorre); // a
-		System.out.println(caracter);
-		for (int i = indiceRecorre; i >= 0; i--) {
-			palinReverse = palinReverse.concat(palin.valueOf(caracter));
-//			 System.out.println(caracter);
-//			 System.out.println(palinReverse);
-//			 System.out.println(indiceRecorre);
-			indiceRecorre--;
-			if (indiceRecorre >= 0) {
-				caracter = palin.charAt(indiceRecorre);
-			}
-		}
-//		System.out.println(palinReverse);
-		imprimePalindromo(palin,palinReverse);
-
+		int indiceRecorre = palin.length() - 1; 
+		char caracter = palin.charAt(indiceRecorre); 
+		palinReverse= crearInverso(palin,palinReverse,indiceRecorre,caracter);
+		//Llamadas a métodos
+		
+		revisaPalindromo(palin,palinReverse);
 	}
 	
-	public static void imprimePalindromo (String palindrom, String palindromInverso) {
+	public static void revisaPalindromo (String palindrom, String palindromInverso) {
 		if (palindrom.equals(palindromInverso)) {
 			System.out.println("Es un palindromo");
 		} else {
 			System.out.println("No es un palindromo");
 		}
+	}
+	
+	public static String crearInverso (
+			String palindromo, 
+			String palindromoReverse, 
+			int indice , char letra ) {
+		
+		for (int i = indice; i >= 0; i--) {
+			palindromoReverse = palindromoReverse
+								.concat(palindromo.valueOf(letra));
+			indice--;
+			if (indice >= 0) {
+				letra = palindromo.charAt(indice);
+			}
+		}
+		
+		return palindromoReverse;
 	}
 
 }
