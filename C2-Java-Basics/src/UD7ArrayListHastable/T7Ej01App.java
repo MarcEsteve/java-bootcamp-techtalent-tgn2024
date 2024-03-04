ce59ed685263952e309c556cd4cea1464dbf3e67
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-03-04 14:01:04 +0100

    Create T7Ej01App.java

---
+++ C2-Java-Basics/src/UD7ArrayListHastable/T7Ej01App.java
@@ -0,0 +1,27 @@
+package UD7ArrayListHastable;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class T7Ej01App {
+
+	public static void main(String[] args) {
+
+		//Declarar variables
+		HashMap<String, Double> alumnosNotaMedia = new HashMap<>();
+		
+		//Acciones: introducir los datos
+		//1: Cuantos alumnos
+		//2: Rellena datos
+		//3: Calcular nota media
+		//4: Mostrar
+		//5: 
+		
+		
+		Scanner intro = new Scanner(System.in);
+		System.out.print("¿Cuántos alumnos y alumnas hay?: ");
+        int numeroAlumnos = intro.nextInt();
+        
+	}
+	
+}
