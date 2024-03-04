29cd7ace02d17afa027e2f9f24ce9d2edeed4068
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-03-04 13:37:26 +0100

    Update UD8Empleado.java

--- C2-Java-Basics/src/UD8POO/UD8Empleado.java
+++ C2-Java-Basics/src/UD8POO/UD8Empleado.java
@@ -7,4 +7,66 @@ public class UD8Empleado {
 	private int edad;
 	private double salario;
 	
+	//Constructores
+	
+	public UD8Empleado() {
+		this.nombre = "";
+		this.apellido = "";
+		this.edad = 0;
+		this.salario = 0.0;
+	}
+
+	public UD8Empleado(String nombre, String apellido, int edad, double salario) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.salario = salario;
+	}
+	
+	//Métodos
+	public boolean plus (double sueldoPlus) {
+		boolean aumento=false;
+		if (edad>40) {
+			salario+=sueldoPlus;
+			aumento=true;
+		} 
+		return aumento;
+	}
+	
+	//Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	
+	
 }
