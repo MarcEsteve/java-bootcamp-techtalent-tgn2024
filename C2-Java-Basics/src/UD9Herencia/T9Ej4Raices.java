e14cead5d46449060d9e74c6c98388d664df95b5
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-04-04 10:07:21 +0200

    Ejercicios del 9

--- C2-Java-Basics/src/UD9Herencia/T9Ej4Raices.java
+++ C2-Java-Basics/src/UD9Herencia/T9Ej4Raices.java
@@ -1,68 +1,52 @@
 package UD9Herencia;
 
 public class T9Ej4Raices {
-    private double a;
-    private double b;
-    private double c;
+	private double a;
+	private double b;
+	private double c;
 
-    // Constructor
-    public T9Ej4Raices(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
+	// Constructor
+	public T9Ej4Raices(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
 
-    // Método para obtener el discriminante
-    public double getDiscriminante() {
-        return Math.pow(b, 2) - 4 * a * c;
-    }
+	// Método para obtener el discriminante
+	public double getDiscriminante() {
+		return Math.pow(b, 2) - 4 * a * c;
+	}
 
-    // Método para comprobar si tiene dos soluciones
-    public boolean tieneRaices() {
-        return getDiscriminante() >= 0;
-    }
+	// Método para comprobar si tiene dos soluciones
+	public boolean tieneRaices() {
+		return getDiscriminante() > 0;
+	}
 
-    // Método para comprobar si tiene una única solución
-    public boolean tieneRaiz() {
-        return getDiscriminante() == 0;
-    }
+	// Método para comprobar si tiene una única solución
+	public boolean tieneRaiz() {
+		return getDiscriminante() == 0;
+	}
 
-    // Método para calcular e imprimir las posibles soluciones
-    public void calcular() {
-        double discriminante = getDiscriminante();
-        if (tieneRaices()) {
-            double raiz1 = (-b + Math.sqrt(discriminante)) / (2 * a);
-            double raiz2 = (-b - Math.sqrt(discriminante)) / (2 * a);
-            System.out.println("Las soluciones son: " + raiz1 + " y " + raiz2);
-        } else if (tieneRaiz()) {
-            double raizUnica = -b / (2 * a);
-            System.out.println("La única solución es: " + raizUnica);
-        } else {
-            System.out.println("No tiene soluciones reales.");
-        }
-    }
-
-    // Método para imprimir las dos posibles soluciones
-    public void obtenerRaices() {
-        double discriminante = getDiscriminante();
-        if (tieneRaices()) {
-            double raiz1 = (-b + Math.sqrt(discriminante)) / (2 * a);
-            double raiz2 = (-b - Math.sqrt(discriminante)) / (2 * a);
-            System.out.println("Las raíces son: " + raiz1 + " y " + raiz2);
-        } else {
-            System.out.println("No tiene raíces reales.");
-        }
-    }
-
-    // Método para imprimir la única raíz
-    public void obtenerRaiz() {
-        double discriminante = getDiscriminante();
-        if (tieneRaiz()) {
-            double raizUnica = -b / (2 * a);
-            System.out.println("La única raíz es: " + raizUnica);
-        } else {
-            System.out.println("No tiene una única raíz real.");
-        }
-    }
-
+	// Método para calcular e imprimir las posibles soluciones
+	public void calcular() {
+		if (tieneRaices()) {
+			obtenerRaices();
+		} else if (tieneRaiz()) {
+			obtenerRaiz();
+		} else {
+			System.out.println("No tiene soluciones reales.");
+		}
+	}
+	// Método para imprimir las dos posibles soluciones
+	public void obtenerRaices() {
+		double discriminante = getDiscriminante();
+		double raiz1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+		double raiz2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+		System.out.println("Las raíces son: " + raiz1 + " y " + raiz2);
+	}
+	// Método para imprimir la única raíz
+	public void obtenerRaiz() {
+			double raizUnica = -b / (2 * a);
+			System.out.println("La única raíz es: " + raizUnica);
+	}
 }
--- C2-Java-Basics/src/UD9Herencia/T9Ej4RaicesMain.java
+++ C2-Java-Basics/src/UD9Herencia/T9Ej4RaicesMain.java
@@ -4,13 +4,24 @@ public class T9Ej4RaicesMain {
 
 	public static void main(String[] args) {
 		// Ejemplo de uso
-		T9Ej4Raices ecuacion = new T9Ej4Raices(1, -3, 2);
-        System.out.println("Discriminante: " + ecuacion.getDiscriminante());
-        System.out.println("¿Tiene raíces? " + ecuacion.tieneRaices());
-        System.out.println("¿Tiene raíz única? " + ecuacion.tieneRaiz());
+		T9Ej4Raices ecuacion = new T9Ej4Raices(9, 1, 13);
+//        System.out.println("Discriminante: " + ecuacion.getDiscriminante());
+//        System.out.println("¿Tiene raíces? " + ecuacion.tieneRaices());
+//        System.out.println("¿Tiene raíz única? " + ecuacion.tieneRaiz());
         ecuacion.calcular();
-        ecuacion.obtenerRaices();
-        ecuacion.obtenerRaiz();
+        	
+//        	Con dos soluciones reales:
+//        	Coeficientes: a = 1, b = -3, c = 2
+//        	Coeficientes: a = 2, b = 5, c = -3
+//        	Coeficientes: a = 1, b = -6, c = 9
+//        	Con una solución real:
+//        	Coeficientes: a = 1, b = -4, c = 4
+//        	Coeficientes: a = 2, b = -8, c = 8
+//        	Coeficientes: a = 3, b = -18, c = 27
+//        	Sin soluciones reales:
+//        	Coeficientes: a = 1, b = 2, c = 3
+//        	Coeficientes: a = 4, b = 2, c = 1
+//        	Coeficientes: a = 1, b = 1, c = 1
 	}
 
 }
