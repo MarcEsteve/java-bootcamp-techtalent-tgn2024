0d4738a76b6599123b8640d4948d02aa4d3b0a7f
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-04-11 10:48:15 +0200

    Cambios DDL

--- C3-Bases-Datos/UD14-DDL/consultas-heidi.sql
+++ C3-Bases-Datos/UD14-DDL/consultas-heidi.sql
@@ -8,4 +8,7 @@ CREATE TABLE usuarios
 	PRIMARY KEY (id));
 CREATE TABLE ciudad1 (nombre CHAR(20)NOT NULL, poblacion INT NULL);
 
-SHOW COLUMNS 
+SHOW COLUMNS FROM gente;
+
+DROP DATABASE [IF EXISTS] gente;
+
