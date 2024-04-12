10f2048cda9522d25c2374fdd3e269132c8421e2
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-04-12 11:15:23 +0200

    Update T14ej2biblio.sql

--- C3-Bases-Datos/UD14-DDL/T14ej2biblio.sql
+++ C3-Bases-Datos/UD14-DDL/T14ej2biblio.sql
@@ -7,3 +7,34 @@ CREATE TABLE editorial (
     direccion VARCHAR(60),
     telefono VARCHAR(15),
     PRIMARY KEY (claveditorial));
+
+CREATE TABLE tema (
+    clavetema SMALLINT NOT NULL,
+    nombre VARCHAR(40),
+    PRIMARY KEY (clavetema));
+
+CREATE TABLE autor (
+    claveautor SMALLINT NOT NULL,
+    nombre VARCHAR(60),
+    PRIMARY KEY (claveautor));
+
+CREATE TABLE socio (
+    clavesocio SMALLINT NOT NULL,
+    nombre VARCHAR(60),
+    direccion VARCHAR(60),
+    telefono VARCHAR(15),
+    categoria CHAR,
+    PRIMARY KEY (clavesocio));
+
+CREATE TABLE libro (
+        clavelibro INT NOT NULL, 
+        titulo VARCHAR(60), 
+        idioma VARCHAR(15), 
+        formato VARCHAR(15),    
+        claveditorial SMALLINT, 
+    PRIMARY KEY (clavelibro), 
+    KEY (claveditorial),
+    FOREIGN KEY (claveditorial) 
+    REFERENCES editorial (claveditorial) 
+    ON DELETE SET NULL
+    ON UPDATE CASCADE);
