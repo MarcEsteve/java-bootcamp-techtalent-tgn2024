002c4868a444539bb3b38f45ea29d811ddaf7c5b
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-04-12 10:55:50 +0200

    Update T14ej2biblio.sql

--- C3-Bases-Datos/UD14-DDL/T14ej2biblio.sql
+++ C3-Bases-Datos/UD14-DDL/T14ej2biblio.sql
@@ -1,3 +1,9 @@
 CREATE DATABASE biblio;
 USE biblio;
 
+CREATE TABLE editorial (
+    claveditorial SMALLINT NOT NULL,
+    nombre VARCHAR(60),
+    direccion VARCHAR(60),
+    telefono VARCHAR(15),
+    PRIMARY KEY (claveditorial));
