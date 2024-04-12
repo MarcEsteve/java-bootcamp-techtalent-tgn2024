9f21d1cc9c27eb860dfcf79dcf8b82e9edb902bb
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-04-12 10:27:31 +0200

    Update T14ej1meteo.sql

--- C3-Bases-Datos/UD14-DDL/T14ej1meteo.sql
+++ C3-Bases-Datos/UD14-DDL/T14ej1meteo.sql
@@ -1,3 +1,20 @@
 CREATE DATABASE meteo;
 USE meteo;
-CREATE TABLE estacion (id )
+CREATE TABLE estacion (
+    id MEDIUMINT UNSIGNED NOT NULL,
+    lat VARCHAR(14) NOT NULL,
+    longitud VARCHAR(15) NOT NULL,
+    alt MEDIUMINT NOT NULL,
+    PRIMARY KEY (id));
+
+CREATE TABLE muestra (
+    idestacion MEDIUMINT UNSIGNED NOT NULL,
+    fecha DATE NOT NULL,
+    tempmin TINYINT,
+    tempmax TINYINT,
+    precipita SMALLINT UNSIGNED,
+    hummin TINYINT UNSIGNED,
+    hummax TINYINT UNSIGNED,
+    velmin SMALLINT UNSIGNED,
+    velmax SMALLINT UNSIGNED,
+    KEY (idestacion), FOREIGN KEY (idestacion) REFERENCES estacion (id) ON DELETE NO ACTION ON UPDATE CASCADE);
