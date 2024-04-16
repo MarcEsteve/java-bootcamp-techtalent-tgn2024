a34d24c1d3eb4828f9bf2b881b347403feeb8b3e
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-04-16 09:50:58 +0200

    Update T15ej1directores.sql

--- C3-Bases-Datos/UD15-DML/T15ej1directores.sql
+++ C3-Bases-Datos/UD15-DML/T15ej1directores.sql
@@ -1,5 +1,5 @@
 INSERT INTO despachos (capacidad) VALUES 
-    (3),(5),(8), (16),(4), (7),(9),(3),(11),(33);
+    (3),(5),(8),(16),(4),(7),(9),(3),(11),(33);
 -- Dos ventajas del auto_increment
 -- 1. Eficiencia en la insercción deatos en la instrucción SQL
 -- 2. Elimina la probabilidad de los errores
