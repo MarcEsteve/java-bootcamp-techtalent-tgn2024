bb19f2ecb776b9d07ec8017830f9122adae76c77
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-04-29 11:34:41 +0200

    Ejercicios T16

--- C3-Bases-Datos/UD16-DML2/T16ej1tiendainfo.sql
+++ C3-Bases-Datos/UD16-DML2/T16ej1tiendainfo.sql
@@ -62,4 +62,58 @@ INSERT INTO articulos (nombre, precio, fabricante) VALUES
 SELECT nombre, precio FROM articulos WHERE precio >= 180 ORDER BY precio DESC, nombre ASC;
 -- 1.10, 1.11
 SELECT articulos.nombre AS "Artículo", fabricantes.nombre AS "Fabricante", precio AS "Precio de artículos por cada fabricante" FROM articulos, fabricantes WHERE articulos.fabricante = fabricantes.codigo;
-SELECT articulos.nombre AS "Artículo", fabricantes.nombre AS "Fabricante", precio AS "Precio de artículos por cada fabricante" FROM articulos INNER JOIN fabricantes ON articulos.fabricante = fabricantes.codigo ORDER BY fabricantes.nombre;
+SELECT fabricantes.nombre AS "Fabricante", articulos.nombre AS "Artículo",  precio AS "Precio de artículos por cada fabricante" FROM articulos INNER JOIN fabricantes ON articulos.fabricante = fabricantes.codigo ORDER BY fabricantes.nombre;
+
+-- 1.12 Obtener el precio medio de los productos de cada fabricante, mostrando solo los códigos de fabricante.
+
+SELECT fabricante AS "Código fabricante", ROUND(AVG(precio),2) AS "Media de precios" FROM articulos GROUP BY fabricante ORDER BY ROUND(AVG(precio),2);
+
+-- 1.13
+SELECT f.nombre AS "Nombre fabricante", ROUND(AVG(a.precio),2) AS "Media de precios" FROM articulos a INNER JOIN fabricantes f
+ON a.fabricante = f.codigo 
+GROUP BY a.fabricante ORDER BY ROUND(AVG(a.precio),2);
+
+-- 1.14 Obtener los nombres de los fabricantes que ofrezcan productos cuyo precio medio sea mayor o igual a 150
+SELECT f.nombre AS "Fabricantes AVG(a.precio) >= 150", ROUND(AVG(a.precio),2) AS "Precio Medio"
+FROM articulos a INNER JOIN fabricantes f 
+ON a.fabricante = f.codigo
+GROUP BY f.nombre
+HAVING AVG(a.precio) >= 150
+ORDER BY AVG(a.precio);
+
+--1.15
+SELECT nombre, precio FROM articulos
+WHERE precio = (SELECT MIN(precio) FROM articulos);
+
+SELECT nombre, precio FROM articulos ORDER BY precio LIMIT 1;
+
+--1.16
+-- 1.16 Obtener una lista con el nombre y precio de los artículos más caros de cada proveedor (incluyendo el nombre del proveedor).
+SELECT a.nombre AS "Articulo más caro", a.precio, f.nombre AS "por Fabricante"
+FROM articulos a, fabricantes f
+WHERE a.fabricante = f.codigo AND a.precio = 
+(
+    SELECT MAX(ar.precio)
+    FROM articulos ar
+    WHERE ar.fabricante = f.codigo
+)
+ORDER BY a.fabricante;
+
+--1.17
+INSERT INTO articulos (nombre, precio, fabricante) 
+VALUES ('Altavoces LG', 69.99, 2);
+
+--1.18 *Uso del SELECT
+UPDATE articulos 
+SET nombre = 'Impresora Laser'
+WHERE codart = 8;
+
+-- 1.19 --
+UPDATE articulos 
+SET precio = precio * 0.9;
+
+-- 1.20
+UPDATE articulos
+SET precio = precio - 10
+WHERE precio >= 120;
+
