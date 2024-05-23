f0d3542a0e60f2f2585d9891d077761ba1ce4266
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-05-23 11:21:52 +0200

    ConversorDivisa APP MVC ejemplo

--- C4-MavenProject3/src/main/java/com/ejemplomvc/appmain/ConversorApp.java
+++ C4-MavenProject3/src/main/java/com/ejemplomvc/appmain/ConversorApp.java
@@ -1,11 +1,33 @@
 package com.ejemplomvc.appmain;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import com.ejemplomvc.controllers.Controlador;
+import com.ejemplomvc.models.Modelo;
+import com.ejemplomvc.views.Vista;
+
 public class ConversorApp {
 
 	public static void main(String[] args) {
 		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO: handle exception
+		}catch (ClassNotFoundException e) {
+			// TODO: handle exception
+		}catch (InstantiationException e) {
+			// TODO: handle exception
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
 		
-
+		Modelo modelo = new Modelo();
+		Vista vista = new Vista();
+		Controlador controlador = new Controlador(modelo, vista);
+		controlador.iniciarVista();
+		
 	}
 
 }
--- C4-MavenProject3/src/main/java/com/ejemplomvc/controllers/Controlador.java
+++ C4-MavenProject3/src/main/java/com/ejemplomvc/controllers/Controlador.java
@@ -1,5 +1,69 @@
 package com.ejemplomvc.controllers;
 
-public class Controlador {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
 
+import com.ejemplomvc.models.Modelo;
+import com.ejemplomvc.views.Vista;
+
+
+public class Controlador implements ActionListener{
+	private Modelo modelo;
+	private Vista vista;
+	private Double cantidad;
+	
+	public Controlador(Modelo modelo, Vista vista) {
+		this.modelo = modelo;
+		this.vista = vista;
+		this.vista.euros.addActionListener(this);
+		this.vista.dolares.addActionListener(this);
+	}
+	
+	public void iniciarVista() {
+		vista.setTitle("Conversor");
+		vista.pack();
+		vista.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		vista.setLocationRelativeTo(null);
+		vista.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent evento) {
+
+		if (vista.euros == evento.getSource()) {
+			
+			if (!"".equals(vista.campoTexto.getText())) {
+				try {
+					
+					cantidad = Double.parseDouble(vista.campoTexto.getText());
+					modelo.setCantidad(cantidad);
+					modelo.setMoneda(0.92);
+					modelo.convertirDolarEuro();
+					vista.lResultado.setText(modelo.getResultado().toString());
+					
+				} catch (NumberFormatException e) {
+					vista.lResultado.setText("Introduzca una cantidad válida...");
+				}
+			}
+			
+		} else if (vista.dolares == evento.getSource()){
+			if (!"".equals(vista.campoTexto.getText())) {
+				try {
+					
+					cantidad = Double.parseDouble(vista.campoTexto.getText());
+					modelo.setCantidad(cantidad);
+					modelo.setMoneda(1.08);
+					modelo.convertirEuroDolar();;
+					vista.lResultado.setText(modelo.getResultado().toString());
+					
+				} catch (NumberFormatException e) {
+					vista.lResultado.setText("Introduzca una cantidad válida...");
+				}
+			}
+			
+			
+		}
+		
+	}
 }
