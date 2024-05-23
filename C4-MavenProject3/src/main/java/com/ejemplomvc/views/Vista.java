72111b4a6205ec30dd479465eb66a117bbf4bb1f
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-05-23 10:31:45 +0200

    Create Vista.java

---
+++ C4-MavenProject3/src/main/java/com/ejemplomvc/views/Vista.java
@@ -0,0 +1,27 @@
+package com.ejemplomvc.views;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Vista extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+	public JButton euros, dolares;
+	private JPanel panelB, panelR;
+	public JLabel lResultado;
+	public JTextField campoTexto;
+	
+	public Vista() {
+		getContentPane().setLayout(new BorderLayout());
+		
+		panelB = new JPanel();
+		panelB.setLayout(new FlowLayout());
+		
+		panelR = new JPanel();
+		panelR.setLayout(new FlowLayout());
+		
+		euros= 
+		
+	}
+
+}
