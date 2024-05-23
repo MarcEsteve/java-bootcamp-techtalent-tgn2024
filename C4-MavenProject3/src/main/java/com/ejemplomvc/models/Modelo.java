3372b4aeafa32f15efa03bd96e11fbf63b618ca2
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-05-23 10:26:10 +0200

    Update Modelo.java

--- C4-MavenProject3/src/main/java/com/ejemplomvc/models/Modelo.java
+++ C4-MavenProject3/src/main/java/com/ejemplomvc/models/Modelo.java
@@ -1,5 +1,28 @@
 package com.ejemplomvc.models;
 
 public class Modelo {
+	private Double moneda;
+	private Double cantidad;
+	private Double resultado;
+	
+	public void setMoneda(Double moneda) {
+		this.moneda = moneda;
+	}
+	public void setCantidad(Double cantidad) {
+		this.cantidad = cantidad;
+	}
+	public Double getResultado() {
+		return resultado;
+	}
+	public void convertirDolarEuro() {
+		resultado= cantidad*moneda;
+	}
+	public void convertirEuroDolar() {
+		resultado= cantidad*moneda;
+	}
+	
+	
+	
+	
 
 }
