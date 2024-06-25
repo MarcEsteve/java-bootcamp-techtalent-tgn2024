0f51f42a9c03ee5b00acaa57bda64651f21bc36c
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-06-25 12:58:58 +0200

    terminos como compoentes extra de ejemplo

--- C5-Frontend/Angular/my-app/src/app/app.component.ts
+++ C5-Frontend/Angular/my-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { AboutComponent } from './pages/about/about.component';
+import { TermsComponent } from './pages/terms/terms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboutComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboutComponent, TermsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
