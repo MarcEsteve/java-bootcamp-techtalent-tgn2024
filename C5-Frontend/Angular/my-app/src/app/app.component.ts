92f4e0b8a108183efb99cee11f7dd4c205492cf0
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-06-25 12:51:25 +0200

    ABOUT component

--- C5-Frontend/Angular/my-app/src/app/app.component.ts
+++ C5-Frontend/Angular/my-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
