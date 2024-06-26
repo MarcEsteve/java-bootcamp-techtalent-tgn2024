ea74a5f3a828c4afc5a89c52977a0786af1fd613
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-06-26 13:49:30 +0200

    todooo

---
+++ C5-Frontend/Angular/webtgn2024/src/app/pepito/pepito.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pepito',
+  standalone: true,
+  imports: [],
+  templateUrl: './pepito.component.html',
+  styleUrl: './pepito.component.css'
+})
+export class PepitoComponent {
+
+}
