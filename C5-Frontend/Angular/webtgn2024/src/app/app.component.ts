e57e0617a704cccd1a94e1411c1ae1153304418e
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-06-26 13:03:07 +0200

    componentes app web angular

---
+++ C5-Frontend/Angular/webtgn2024/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TermsComponent } from './pages/terms/terms.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AboutComponent, TermsComponent, PortfolioComponent],
+  templateUrl: './app.component.html',
+  styles: ''
+})
+export class AppComponent {
+  title = 'Mi primera app';
+}
---
+++ C5-Frontend/Angular/webtgn2024/src/app/pages/portfolio/portfolio.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      portfolio workkkdsdss
+    </p>
+  `,
+  styles: `
+  p {
+    color:red;
+  }
+  `
+})
+export class PortfolioComponent {
+
+}
