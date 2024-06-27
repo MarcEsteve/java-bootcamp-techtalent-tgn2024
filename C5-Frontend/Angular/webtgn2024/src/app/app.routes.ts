0b63cf2c21618d5994684d55bf77160968e5b644
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-06-27 13:59:36 +0200

    interpolacion en DATA BINDING

--- C5-Frontend/Angular/webtgn2024/src/app/app.routes.ts
+++ C5-Frontend/Angular/webtgn2024/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AboutComponent } from './pages/about/about.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { TermsComponent } from './pages/terms/terms.component';
 import { Pagina404Component } from './pages/pagina-404/pagina-404.component';
+import { InterpolationComponent } from './databinding/interpolation/interpolation.component';
 
 export const routes: Routes = [
     {path: '', component: AboutComponent},
     {path: 'portfolio', component: PortfolioComponent},
+    {path: 'interpol', component: InterpolationComponent},
     {path: 'terminos', component: TermsComponent},
     {path: 'about', redirectTo: '', pathMatch: 'full'},
     {path: '**', component: Pagina404Component}
---
+++ C5-Frontend/Angular/webtgn2024/src/app/databinding/interpolation/interpolation.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation',
+  standalone: true,
+  imports: [],
+  templateUrl: './interpolation.component.html',
+  styleUrl: './interpolation.component.css',
+})
+export class InterpolationComponent {
+  actor: any = {
+    apellidos: 'De Niro',
+    nombre: 'Robert',
+    fechaNacimiento: new Date(1943, 7, 17),
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  getEdad(): number {
+    const edad: number =
+      (new Date().getTime() - this.actor.fechaNacimiento.getTime()) /
+      (365 * 24 * 60 * 60 * 1000);
+    return Math.ceil(edad);
+  }
+}
