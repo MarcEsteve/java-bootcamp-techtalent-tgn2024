86bb7c995c6b3eef04e1f931437195095fca82e4
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-06-27 10:09:00 +0200

    404component

---
+++ C5-Frontend/Angular/webtgn2024/src/app/pages/404/404.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-404',
+  standalone: true,
+  imports: [],
+  templateUrl: './404.component.html',
+  styles: ``
+})
+export class 404Component {
+
+}
