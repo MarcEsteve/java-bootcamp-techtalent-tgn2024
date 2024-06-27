78c7f94a9ecb3e20be1c6b81dd1c7e380bf11867
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-06-27 10:28:32 +0200

    importar RouterOutle, RouterLink para el uso de routeLink en vez de HREFs

--- C5-Frontend/Angular/webtgn2024/src/app/shared/footer/footer.component.ts
+++ C5-Frontend/Angular/webtgn2024/src/app/shared/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
--- C5-Frontend/Angular/webtgn2024/src/app/shared/header/header.component.ts
+++ C5-Frontend/Angular/webtgn2024/src/app/shared/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
