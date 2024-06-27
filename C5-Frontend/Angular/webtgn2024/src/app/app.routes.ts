2c196d088f094a5f97f9210feb1eee37d60c3aef
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-06-27 10:13:16 +0200

    404 y más componentes y rutas

--- C5-Frontend/Angular/webtgn2024/src/app/app.routes.ts
+++ C5-Frontend/Angular/webtgn2024/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { TermsComponent } from './pages/terms/terms.component';
+import { Pagina404Component } from './pages/pagina-404/pagina-404.component';
 
 export const routes: Routes = [
     {path: 'about', component: AboutComponent},
     {path: 'portfolio', component: PortfolioComponent},
     {path: 'terminos', component: TermsComponent},
-    {path: '', redirectTo: 'portfolio', pathMatch: 'full'},
-    {path: '**', component: AboutComponent}
+    {path: '', redirectTo: 'about', pathMatch: 'full'},
+    {path: '**', component: Pagina404Component}
 ];
---
+++ C5-Frontend/Angular/webtgn2024/src/app/pages/pagina-404/pagina-404.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-404',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagina-404.component.html',
+  styles: `
+    h2{
+      text-align:center;
+    }
+  `
+})
+export class Pagina404Component {
+
+}
