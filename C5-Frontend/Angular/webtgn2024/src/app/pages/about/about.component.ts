eada9c45d33b1063aa46902182ee84e7bee1e5d6
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-07-02 13:36:43 +0200

    Service de información de la página

--- C5-Frontend/Angular/webtgn2024/src/app/pages/about/about.component.ts
+++ C5-Frontend/Angular/webtgn2024/src/app/pages/about/about.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { InfoPaginaService } from '../../servicios/info-pagina.service';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
- nombre="Angelina";
+  nombre = 'Angelina';
+
+  constructor(public infoPaginaService: InfoPaginaService) {
+    //Inyección de dependencia del servicio
+  }
 }
---
+++ C5-Frontend/Angular/webtgn2024/src/app/servicios/info-pagina.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  constructor() {
+    console.log("Servicio de infoPagina listo");
+   }
+}
