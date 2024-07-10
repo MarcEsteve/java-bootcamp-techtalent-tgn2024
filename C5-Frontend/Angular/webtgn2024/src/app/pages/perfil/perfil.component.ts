2773f8bcbd500f36c95acf99ffe3ded27cf7fc0c
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2024-07-10 03:18:57 +0200

    Añadir * para ocultar contraseña

--- C5-Frontend/Angular/webtgn2024/src/app/pages/perfil/perfil.component.ts
+++ C5-Frontend/Angular/webtgn2024/src/app/pages/perfil/perfil.component.ts
@@ -1,25 +1,28 @@
-import { CommonModule, TitleCasePipe } from '@angular/common';
+import { CommonModule, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../servicios/auth.service';
 import { RouterLink } from '@angular/router';
 import { User } from '../../interfaces/user.interface';
+import { PasswordPipe } from '../../pipes/password.pipe';
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [CommonModule, TitleCasePipe, RouterLink],
+  imports: [CommonModule, TitleCasePipe, RouterLink, PasswordPipe, UpperCasePipe],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
 export class PerfilComponent implements OnInit {
 
   usuario: User | undefined;
+  psw: string | any = '';
 
   constructor(public authService: AuthService) {}
 
   ngOnInit(): void {
     if (this.authService.isLoggedIn()) {
       this.usuario = this.authService.getUsuarioDB();
+      this.psw = this.usuario?.pass;
     }
   }
 
---
+++ C5-Frontend/Angular/webtgn2024/src/app/pipes/password.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'password',
+  standalone: true
+})
+export class PasswordPipe implements PipeTransform {
+  transform(value: string, replaceChar?: string): any {
+    if (value === undefined) {
+      return value;
+    }
+    // Replace with the specified character
+    if (replaceChar) {
+      return replaceChar.repeat(value.length);
+    }
+    // Replace value with asterisks
+    return '*'.repeat(value.length);
+  }
+}
